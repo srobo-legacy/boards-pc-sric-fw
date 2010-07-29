f62a77c27cc6c56fc4e65e7083f6e5bd2bec2d95
Robert Spanton <rspanton@zepler.net>
2010-07-29 02:45:55 +0100

    Source XT2 on boot
    When starting from a crystal, one has to repeatedly kick it until it's
    happy.  See "Sourcing MCLK from a Crystal" in the family user's guide.

--- main.c
+++ main.c
@@ -16,12 +16,41 @@
 #include <io.h>
 #include "leds.h"
 
+#define set_field( x, val, mask ) do { x = (x & ~mask) | val; } while(0)
+
+/* Kick the XT2 crystal until it starts oscillating */
+void xt2_boot( void )
+{
+	/* Use red LED to diagnose oscillator fault */
+	led_red(1);
+
+	do {
+		uint8_t i;
+
+		/* Switch the XT2 osc on */
+		BCSCTL1 &= ~XT2OFF;
+		set_field( BCSCTL2, SELM_XT2CLK, SELM_3 );
+
+		/* Clear flag to allow oscillator test */
+		IFG1 &= ~OFIFG;
+
+		/* Wait at least 50µs (currently on ~800kHz DCO) */
+		for( i=0; i<40; i++ )
+			nop();
+	} while( IFG1 & OFIFG );
+
+	led_red(0);
+}
+
 void init( void )
 {
 	/* Tristate all pins */
 	P1DIR = P2DIR = P3DIR = P4DIR = P5DIR = P6DIR = 0;
 
 	leds_init();
+
+	/* Source MCLK from XT2 */
+	xt2_boot();
 }
 
 int main( void )
